059cae67f5f42f9ff386044c866f284c586531a3
Diffa <ddiffa2@gmail.com>
2020-07-14 22:02:13 +0700

    add count down timer

---
+++ lib/timer.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+import 'timer_model.dart';
+
+class CountDownTimer {
+  double _radius = 1;
+  bool _isActive = true;
+  Timer timer;
+  Duration _time;
+  Duration _fullTime;
+
+  String returnTime(Duration t) {
+    String minutes = (t.inMinutes < 10)
+        ? '0' + t.inMinutes.toString()
+        : t.inMinutes.toString();
+    int numSeconds = t.inSeconds - (t.inMinutes * 60);
+    String seconds =
+        (numSeconds < 10) ? '0' + numSeconds.toString() : numSeconds.toString();
+    String formattedTime = minutes + ":" + seconds;
+    return formattedTime;
+  }
+
+  Stream<TimerModel> stream() async*{
+
+    //yield* statement to deliver a result. T0 make it simple, it's like a return statement,
+    // but it doesn't end the function.
+    // You have to use "*" sign after yield because we are returning a Stream.
+
+    //yield* Stream.periodic() is a constructor creating a Stream that emits events at the intervals specified
+    // in the first parameter. In our code, this will emit a value every 1 second.
+    yield* Stream.periodic(Duration(seconds: 1), (int a) {
+
+      //We declare a String called time and check whether the _isActive field is true, as follows :
+      String time;
+      if(this._isActive){
+
+        // If it is, we decrease the value of time by 1 second (it's a countdown, after all), like this:
+        _time = _time - Duration(seconds: 1);
+
+        // we also update the _radius value. This is the remaining time divided by the total time, as follows:
+        // This value goes from 1, at the beginning of the countdown, to 0 at the end of the countdown
+        _radius = _time.inSeconds / _fullTime.inSeconds;
+
+        // We check whether the _time field got down to 0 and if it did, we change the value of _isActive to false
+        // to stop the countdown, as follows:
+        if(_time.inSeconds <=0){
+          _isActive = false;
+        }
+      }
+
+      //we call the returnTime method to transform the remaining Duration into a String, like This :
+      time = returnTime(_time);
+      //Finally, we return a TimerModel Object containing the time String and the _radius double like this
+      return TimerModel(time, _radius);
+
+      //So this function returns a Stream of TimerModel, decrementing the duration every second.
+    });
+  }
+}
---
+++ lib/timer_model.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TimerModel{
+  String time;
+  double percent;
+
+  TimerModel(this.time, this.percent);
+
+
+}
